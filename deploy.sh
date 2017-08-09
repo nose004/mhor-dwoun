57b0a8116175679dfc0be63bf0edede87ea830ea
Ryosuke ONOSE <dev@onose.ws>
2017-08-09 16:48:49 +0700

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ gulp import
 git branch -f gh-pages
 git checkout gh-pages
 git add -f *.html
-git commit -m "deploy"
+git commit -m "deploy `date '+%m/%d/%y-%H:%M:%S'`"
 git push origin gh-pages --force
 git checkout master
